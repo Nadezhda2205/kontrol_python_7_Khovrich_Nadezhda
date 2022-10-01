1a0a2731680400c279c5dde6446becc54d131054
Nadezhda Khovrich <hope28281@gmail.com>
2022-10-01 15:03:47 +0600

    Реализовано редактирование, удаление

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from guestbook.views import guestbooks_view, guestbooks_add_view
+from guestbook.views import guestbooks_view, guestbooks_add_view, guestbooks_edit_view, guestbooks_delete_view, confirm_delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', guestbooks_view, name='guestbooks'),
     path('guestbooks/add/', guestbooks_add_view, name='guestbooks_add'),
+    path('guestbooks/edit/<int:pk>', guestbooks_edit_view, name='guestbooks_edit'),
+    path('guestbooks/delete/<int:pk>', guestbooks_delete_view, name='guestbooks_delete'),
+    path('guestbooks/confirm/delete/<int:pk>', confirm_delete, name='confirm_delete'),
 ]
--- guestbook/views.py
+++ guestbook/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from guestbook.models import Guestbook
 from django.core.handlers.wsgi import WSGIRequest
 from guestbook.forms import GuestbookForm
@@ -32,3 +32,36 @@ def guestbooks_add_view(request:WSGIRequest):
     return render(request=request, template_name='guestbooks_add.html', context=context)
 
 
+def guestbooks_edit_view(request, pk):
+    if request.method == 'POST':
+        guestbook = get_object_or_404(Guestbook, pk=pk)
+        form = GuestbookForm(request.POST, instance=guestbook)
+        if not form.is_valid():
+            context = {
+            'form': form
+            }  
+            return render(request=request, template_name='guestbooks_edit.html', context=context)
+
+        form.save()
+        return redirect('guestbooks')
+        
+    guestbook = get_object_or_404(Guestbook, pk=pk)
+    form = GuestbookForm(instance=guestbook)
+    context = {
+        'form': form
+    }
+    return render(request=request, template_name='guestbooks_edit.html', context=context)
+
+
+def guestbooks_delete_view(request, pk):
+    guestbook = get_object_or_404(Guestbook, pk=pk)
+    context = {
+        'guestbook': guestbook
+    }
+    return render(request, 'confirm_delete.html', context)
+
+
+def confirm_delete(request, pk):
+    guestbook = get_object_or_404(Guestbook, pk=pk)
+    guestbook.delete()
+    return redirect('guestbooks')
