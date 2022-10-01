390785bec89246134fbb8f7570ed00acc7b19c8f
Nadezhda Khovrich <hope28281@gmail.com>
2022-10-01 12:42:48 +0600

    Добавлена guests.html

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
+from guestbook.views import guestsbook_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', guestsbook_view, name='guestsbook'),
 ]
--- guestbook/views.py
+++ guestbook/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from guestbook.models import Guestbook
 
-# Create your views here.
+def guestsbook_view(request):
+    guests = Guestbook.objects.all()
+    context = {
+        'guests': guests
+    }
+    return render(request=request, template_name='guests.html', context=context)
+
