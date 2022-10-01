5e206ad06a721017d315d7d55267b309c467a201
Nadezhda Khovrich <hope28281@gmail.com>
2022-10-01 15:59:30 +0600

    Откорректирована миграция, фикстура

--- guestbook/forms.py
+++ guestbook/forms.py
@@ -1,14 +1,6 @@
 from guestbook.models import Guestbook
 from django.forms import ModelForm
 
-# class ProductForm(forms.Form):
-#     name = forms.CharField(max_length=100, required=True, label='Наименование')
-#     description = forms.CharField(widget=forms.Textarea, max_length=2000, required=False, label='Описание')
-#     photo = forms.CharField(widget=forms.Textarea, max_length=2000, required=False, label='Фото')
-#     category = forms.ChoiceField(choices=Product.CATEGORY_CHOICES, required=True, label='Категория',)
-#     balance = forms.IntegerField(min_value=0, label='Остаток')
-#     price = forms.DecimalField(max_digits=7, decimal_places=2, label='Стоимость')
-
 
 class GuestbookForm(ModelForm):
     class Meta:
